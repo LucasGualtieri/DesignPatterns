d7dbc137907fe93927823734f72b77657546af49
Lucas Gualtierigit config --global user.email lucasgualtieri.dev@gmail.com <lucasgualtieri.dev@gmail.com>
2024-07-13 06:07:39 -0300

    Create: Projeto Biblioteca
    Refatorando o TP03 com os novos DesignPatterns em mente, especialmente o
    Strategy e o Observer. Mais pra frente também vou adicionar os novos
    features do TP04

---
+++ Biblioteca/compileNrun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create the bin directory if it doesn't exist
+mkdir -p bin
+
+# Compile the Java files
+# javac -d bin -sourcepath . *.java EstruturasDeDados/*.java Entidades/Autores/*.java Entidades/Autores/Indices/*.java Entidades/Livros/*.java Entidades/Livros/Indices/*.java
+# javac -d bin -sourcepath . *.java
+javac -d bin -sourcepath . *.java Lib.java EstruturasDeDados/*.java EstruturasDeDados/Compressao/*.java EstruturasDeDados/Tuplas/*.java Entidades/Livros/*.java Entidades/Livros/Indices/*.java Entidades/Autores/*.java Entidades/Autores/Indices/*.java
+
+echo "Compilation complete."
+
+# Run the main class
+java -cp bin Biblioteca.Principal
