af325c56c1b8eac892ae4afd3393d369f63bf218
Lucas Gualtierigit config --global user.email lucasgualtieri.dev@gmail.com <lucasgualtieri.dev@gmail.com>
2024-07-16 01:30:29 -0300

    Update: Comments describing future implementations

--- src/Observer/Custom/Subject.java
+++ src/Observer/Custom/Subject.java
@@ -5,3 +5,18 @@ public interface Subject {
 	public void removeObserver(Observer observer) throws IllegalArgumentException;
 	public void notifyObservers(Object data);
 } 
+
+// This is supposed to help avoiding to make notifications when the state changes more frequently then the observer need to be updated
+// This how the "Observer CLASS" is implemented in the deprecated java native class 
+//setChanged() { changed = true }
+//
+//notifyObservers(Object arg) {
+//	if (changed) {
+//		for (every observer on the list) {
+//			 call update (this, arg);
+//		}
+//		changed = false
+//	} 
+//}
+//
+//notifyObservers() { notifyObservers(null) }
