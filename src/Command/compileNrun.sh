7b8ef0de683d1a3403d144d984564237ebdde98d
Lucas Gualtierigit config --global user.email lucasgualtieri.dev@gmail.com <lucasgualtieri.dev@gmail.com>
2024-07-22 18:19:14 -0300

    Update: Adding tabs to the directories in copileNrun of command pattern

--- src/Command/compileNrun.sh
+++ src/Command/compileNrun.sh
@@ -5,15 +5,15 @@ mkdir -p bin
 
 # Compile the Java files
 javac -d bin -sourcepath .		\
-  *.java						\
-  Receivers/*.java				\
-  Commands/*.java				\
-  Commands/CeilingFan/*.java	\
-  Commands/GarageDoor/*.java	\
-  Commands/Lights/*.java		\
-  Commands/Stereo/*.java		\
-  Commands/Hottub/*.java		\
-  Commands/TV/*.java			\
+	*.java						\
+	Receivers/*.java			\
+	Commands/*.java				\
+	Commands/CeilingFan/*.java	\
+	Commands/GarageDoor/*.java	\
+	Commands/Lights/*.java		\
+	Commands/Stereo/*.java		\
+	Commands/Hottub/*.java		\
+	Commands/TV/*.java			\
 
 # echo "Compilation complete."
 
